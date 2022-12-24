4e7306293c45456c6b7d4b9480110170c5a76283
yryrgogo <yryrgogo@gmail.com>
2022-12-24 16:11:10 +0900

    :+1: 2.5章 Connect State への遷移

---
+++ src/event.rs
@@ -0,0 +1,7 @@
+/// BGP RFC 8.1
+/// (https://datatracker.ietf.org/doc/html/rfc4271#section-8.1) で
+/// 定義されている列挙型
+#[derive(PartialEq, Eq, Debug, Clone, Hash)]
+pub enum Event {
+    ManualStart,
+}
--- src/peer.rs
+++ src/peer.rs
@@ -1,9 +1,65 @@
+use tracing::{debug, info, instrument};
+
+use crate::config::Config;
+use crate::event::Event;
+use crate::event_queue::EventQueue;
+use crate::state::State;
+
+/// BGP の RFC で示されている実装方針
+/// (https://datatracker.ietf.org/doc/html/rfc4271#section-8) では、
+/// 1つの Peer を1つのイベント駆動ステートマシンとして実装しています。
+/// Peer 構造体は RFC 内で示されている実装方針に従ったイベント駆動ステートマシンです。
+#[derive(Debug)]
+pub struct Peer {
+    state: State,
+    event_queue: EventQueue,
+    config: Config,
+}
+
+impl Peer {
+    pub fn new(config: Config) -> Self {
+        let state = State::Idle;
+        let event_queue = EventQueue::new();
+        Self {
+            state,
+            event_queue,
+            config,
+        }
+    }
+
+    #[instrument]
+    pub fn start(&mut self) {
+        info!("peer is started.");
+        self.event_queue.enqueue(Event::ManualStart);
+    }
+
+    #[instrument]
+    pub async fn next(&mut self) {
+        if let Some(event) = self.event_queue.dequeue() {
+            info!("event is occured, event={:?}.", event);
+            self.handle_event(event).await;
+        }
+    }
+
+    async fn handle_event(&mut self, event: Event) {
+        match &self.state {
+            State::Idle => match event {
+                Event::ManualStart => {
+                    self.state = State::Connect;
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[tokio::test]
     async fn peer_can_transition_to_connect_state() {
-        let config: Config = "64512 1270.0.1 65413 127.0.0.2 active".parse().unwrap();
+        let config: Config = "64512 127.0.0.1 65413 127.0.0.2 active".parse().unwrap();
         let mut peer = Peer::new(config);
         peer.start();
         peer.next().await;
